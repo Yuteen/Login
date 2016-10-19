eb68d3e684a92a530ba456060c570e14e52b7667
nikhare12 <yuteen.maximess@gmail.com>
2016-10-20 04:25:13 +0530

    add php files

---
+++ welcome.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+  <title>WELCOME</title>
+</head>
+
+<body>
+    <form name="welcome" id="welcome" method="post" action="" target="" enctype="multipart/form-data">
+    <div style="border: solid 1px #ccc; margin: 0 auto; background-color: #888888;">
+    <div style="">
+    <ul style="">
+    <li>HOME</li>
+    <li>MESSAGES</li>
+    <li>TWEETS</li>
+    <li>ABOUT US</li>
+    </ul>
+    </div>
+    </div>
+    </form>
+</body>
+
+</html>
